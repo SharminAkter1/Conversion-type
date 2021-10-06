fb070c88fdea531736cffda707d311cf57fe2d7c
Sharmin Akter <72566503+SharminAkter1@users.noreply.github.com>
2021-10-06 08:00:18 +0600

    Completed

---
+++ app.js
@@ -0,0 +1,41 @@
+let val;
+//Number to String
+val = String(55);
+val = String(4+4);
+//Bool to String
+val = String(true);
+//Date to String
+val = String(new Date());
+//Array to String
+val =String([1,3,4,5,6]); 
+
+//toString()
+val = (5).toString();
+val = (true).toString();
+
+//String to Number
+val = Number('54.5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('Hello');
+val = Number([1,2,3]);
+val = parseInt('4.6');
+val = parseFloat('4.6');
+
+
+
+// //Output
+// console.log(val);
+// console.log(typeof val);
+// //console.log(typeof val.lenght);
+// console.log(val.toFixed(2));
+
+const val1 = String(5);
+const val2 = (6);
+const sum = Number(val1 + val2);
+
+console.log(sum);
+console.log(typeof sum);
+
+
